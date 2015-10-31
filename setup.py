7a7683f3a4973921a85205022287e4304c2d7b4d
Lucas H. Xu <xuh@bu.edu>
2015-10-31 16:29:38 -0400

    setup.py and nbaapi.py prototype

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'nbaapi',
+    version = '0.1',
+        packages = ['nbaapi'],
+            install_requires=['pandas>=0.13','requests>=2.2.1'],
+                author = 'Kebi Hong, Zetian Wu, Hang Xu',
+                    url = 'https://github.com/NBA-API/API',
+                        author_email = '',
+                            license = 'MIT'
+                                )
